ad5e922b3a43015f15cf7ebf6cd3ecd014322846
Felype <Felype.f.sposito@gmail.com>
2024-04-30 11:57:56 -0300

    a

---
+++ Aula01/TiposVariaveis.js
@@ -0,0 +1,15 @@
+var nome = 'Felype'; //Declarando a var
+
+let idade = 19
+
+const CPF = '15592792'
+ 
+console.log(`Olá, ${nome}`)
+
+
+//var - Escopo Global
+
+//let - Escopo de Bloco (Não pode redeclarar)
+
+//const - Não pode redeclarar e não podemos alterar o seu valor
+
---
+++ Aula01/main.js
@@ -0,0 +1,20 @@
+var nome_usuario //snake_case
+var nomeUsuario //camelCase
+var NomeUsuario //PascalCase
+//var nome-usuario // kebab-case
+
+nome_usuario = 'Junior';
+nomeUsuario = "Felype";
+NomeUsuario = `Alice`;
+
+console.log('Olá, Mundo!');
+console.log('Olá,', nome_usuario + ' - (para aumentar uma string, coloque o sinal de + e continue como se fosse repetir)')
+console.log('Olá, ' + nomeUsuario)
+console.log(`Olá, ${NomeUsuario}`)
+console.log(`A multiplicação de: 5 x 5 = ${5 * 5}`)
+
+idade = 19
+
+console.log(`Eu sou o ${nomeUsuario} e tenho ${idade} anos de idade.`);
+
+// let idade = 19 (let é o jeito certo de declarar uma variavel)
